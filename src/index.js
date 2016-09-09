9ab2fc25990fd29ef62355d849552941d10f8ad8
GalenWarren <galen.warren@spglobal.com>
2016-09-09 00:40:41 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import couchbase from "couchbase";
+import {EventEmitter} from 'events';
 
-export default class CouchbaseSessionStore {
+export default class CouchbaseSessionStore extends EventEmitter {
 
-  constructor({ cluster, bucket }) {
-    this.cluster = new couchbase.Cluster( cluster );
-    this.bucket = null;
+  constructor({ connect, bucketName, bucketPassword=null, open=true }) {
+    super();
+
+    this.connect = connect;
+    this.bucketName = bucketName;
+    this.bucketPassword = bucketPassword;
+
+    if (open) {
+      this.open()
+    }
+
+  }
+
+  open() {
+
+    return new Promise( (resolve, reject) => {
+
+      this.cluster = new couchbase.Cluster( this.connect );
+      this.bucket = this.cluster.openBucket( this.bucketName, this.password );
+
+      this.bucket.on( "connect", () => {
+        resolve();
+        this.emit( "connect", null );
+      });
+
+      this.bucket.on( "error", err => {
+        reject( err );
+        this.emit( "error", err );
+      });
+
+    });
+
   }
 
   *get( sid ) {
--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 import "babel-polyfill";
+import {EventEmitter} from "events";
 
 import co from "co";
 import sinon from "sinon";
@@ -7,63 +8,117 @@ import mockery from "mockery";
 
 const assert = chai.assert;
 
+const valid = {
+  connect: "couchbase://localhost",
+  bucketName: "session",
+  bucketPassword: "password"
+};
+
+const delay = 100;
+
 /**
-* Prepares the state for tests
+* A mock bucket class
+*/
+class Bucket extends EventEmitter {
+  constructor( valid ) {
+    super();
+
+    setTimeout( () => {
+      console.log("Abc", valid)
+      if (valid) {
+        this.emit( "connect", null );
+      }
+      else {
+        this.emit( "error", "fail" );
+      }
+    }, delay );
+  }
+}
+
+/**
+* A mock cluster class
+*/
+class Cluster  {
+
+  constructor( connect ) {
+    this.valid = connect === valid.connect;
+    this.openBucket = sinon.spy( this.openBucket );
+  }
+
+  openBucket( name, password ) {
+    const bucketValid = this.valid &&
+      (name===valid.bucketName) && (password===valid.bucketPassword);
+    return new Bucket( bucketValid );
+  }
+}
+
+/**
+* Prepare for the tests
 */
 function beforeTests() {
 
-  const bucket = {
-
-  };
-
-  const cluster = {
-    openBucket: sinon.stub().returns( bucket )
-  };
-
-  const couchbase = {
-    Cluster: sinon.stub().returns( cluster )
-  };
-
   mockery.enable({
     useCleanCache: true,
-    warnOnReplace: true,
+    warnOnReplace: false,
     warnOnUnregistered: false
   });
 
-  mockery.registerMock( "couchbase", couchbase );
+  this.couchbase = {
+    Cluster: sinon.spy(Cluster)
+  };
 
-  const CouchbaseSessionStore = require("../src/index").default;
+  mockery.registerMock( "couchbase", this.couchbase );
 
-  this.state = { CouchbaseSessionStore, bucket, cluster, couchbase };
+  this.CouchbaseSessionStore = require("../src/index").default;
 
 }
 
+/**
+* Tear down after tests
+*/
 function afterTests() {
   mockery.disable();
 }
 
+
 describe('CouchbaseSessionStore constructor', function() {
 
-  before( beforeTests );
-  after( afterTests );
+  beforeEach( beforeTests );
+  afterEach( afterTests );
 
-  it('should initialize couchbase cluster and bucket', function( done ) {
+  it('should properly initialize couchbase cluster and bucket when open=true', function( done ) {
 
-    const options = {
-      cluster: "couchbase://localhost",
-      bucket: "session"
-    };
+    const store = new this.CouchbaseSessionStore({
+      connect: valid.connect,
+      bucketName: valid.bucketName,
+      bucketPassword: valid.bucketPassword,
+      open: true
+    });
 
-    const store = new this.state.CouchbaseSessionStore(options);
+    store.on( "connect", () => {
 
-    assert(this.state.couchbase.Cluster.called);
-    assert(this.state.couchbase.Cluster.calledWith( options.cluster ));
+      assert( this.couchbase.Cluster.called);
+      assert( this.couchbase.Cluster.calledWith( options.cluster ));
 
-    //assert(this.state.couchbase.Cluster.called);
-    //assert(this.state.couchbase.Cluster.calledWith( options.cluster ));
+      //assert(this.state.cluster.openBucket.called);
+      //assert(this.state.cluster.openBucket.calledWith( options.bucket ));
 
+      done();
 
-    done();
+    });
+
+  });
+
+  it('should not initialize couchbase cluster and bucket when open=false', function() {
+
+    const store = new this.CouchbaseSessionStore({
+      connect: valid.connect,
+      bucketName: valid.bucketName,
+      bucketPassword: valid.bucketPassword,
+      open: false
+    });
+
+    assert(!this.couchbase.Cluster.called, "Cluster constructor should not be called");
 
   });
 });
